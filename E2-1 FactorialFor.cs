7a445d3185e2ff0ade4d4a110130e470f35b1e49
gllez <42877409+gllez@users.noreply.github.com>
2018-09-28 09:37:26 -0700

    Factorial

---
+++ E2-1 FactorialFor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace E2_1_Factorial
+{
+    public class Factorial
+    {
+        public int facto(int numero)
+        {
+            int fact = 1;
+            for (int i = 1; i <= numero; i++)
+            {
+                fact = fact * i;
+            }
+            return fact;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 6;
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            Factorial factorial = new Factorial();
+            Console.Write("El factorial del numero 6 es: {0}\nEl tiempo transcurrido para realizacion la operacion: {1}", factorial.facto(numero), stopwatch.Elapsed);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ E2-1 Factorialrecursivo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace E2_1_Factorial_recursividad
+{
+    public class Factorial
+    {
+        public int factorialrecursivo(int n)
+        {
+            if (n == 1) { return 1; }
+            else { return n * factorialrecursivo(n - 1); }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            int numero = 6; Factorial factorial = new Factorial();
+            Console.Write("El factorial del numero 6 es: {0}\nEl tiempo de duracion del proceso es de: {1}", factorial.factorialrecursivo(numero), stopwatch.Elapsed);
+            Console.ReadKey();
+        }
+    }
+}

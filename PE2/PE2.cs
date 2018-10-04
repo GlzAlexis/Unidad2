5c34ec6dba1163f59c51c66dc3b4935187d2025c
gllez <42877409+gllez@users.noreply.github.com>
2018-10-04 10:12:01 -0700

    PE2

---
+++ PE2/PE2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibreriaProcesos;
+
+namespace PE2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opc = 0;
+            int Cantidad = 0;
+            try
+            {
+                Console.Write("De la siguientes opciones elija el proceso que desee realizar \n1.- Encontrar el valor maximo del Arreglo \n2.- Encontrar el valor minimo del arreglo \n3.- Invertir la sucesion \n4.- Salir del programa" +
+                "\nTeclee el numero de la opcion que desee: "); opc = Convert.ToInt32(Console.ReadLine()); Console.Clear();
+                Procesos Traer = new Procesos();
+
+                switch (opc)
+                {
+                    case 1:
+                        Console.Write("Escriba la cantidad de datos que desea: "); Cantidad = Convert.ToInt32(Console.ReadLine());
+                        int[] Arreglo = new int[Cantidad];
+                        Arreglo = Traer.Numeros(Cantidad);
+                        Console.Write("\nEl valor minimo es: {0}", Traer.NoMayor(0, Arreglo[0], Arreglo)); Console.ReadKey(); break;
+                    case 2:
+                        Console.Write("Escriba la cantidad de datos que desea: "); Cantidad = Convert.ToInt32(Console.ReadLine());
+                        int[] Arreglo2 = new int[Cantidad];
+                        Arreglo = Traer.Numeros(Cantidad);
+                        Console.Write("\nEl valor minimo es: {0}", Traer.NoMenor(0, Arreglo[0], Arreglo)); Console.ReadKey(); break;
+                    case 3:
+                        Console.Write("Escriba la cantidad de datos que desea: "); Cantidad = Convert.ToInt32(Console.ReadLine());
+                        int[] Arreglo3 = new int[Cantidad];
+                        Arreglo = Traer.Numeros(Cantidad);
+                        Console.Write("\nLa sucesion invertida: "); Traer.Inverso(Cantidad, Arreglo); Console.ReadKey(); break;
+                    case 4:
+                        Console.Write("Presione cualquier tecla para salir"); Console.ReadKey(); break;
+                }
+                Console.WriteLine("Gracias por utilizar el programa"); Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\n"+ e.Message);
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ PE2/Procesos.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaProcesos
+{
+    public class Procesos
+    {
+        public int[] Numeros(int Cantidad)
+        {
+            int[] Arreglo = new int[Cantidad];
+            for (int i = 0; i < Cantidad; i++) { Console.Write("Ingresa un valor {0}: ", (i + 1)); Arreglo[i] = Convert.ToInt32(Console.ReadLine()); }
+            return Arreglo;
+        }
+
+        public int NoMayor(int i, int Mayor, int[] Arreglo)
+        {
+            if (i != Arreglo.Length)
+            {
+                if (Arreglo[i] > Mayor) { Mayor = NoMayor((i + 1), Arreglo[i], Arreglo); }
+                else { Mayor = NoMayor((i + 1), Mayor, Arreglo); }
+            }
+            return Mayor;
+        }
+        public int NoMenor(int i, int Menor, int[] Arreglo)
+        {
+            if (i != Arreglo.Length)
+            {
+                if (Arreglo[i] < Menor) { Menor = NoMenor((i + 1), Arreglo[i], Arreglo); }
+                else { Menor = NoMenor((i + 1), Menor, Arreglo); }
+            }
+            return Menor;
+        }
+        public void Inverso(int i, params int[] Arreglo)
+        {
+            double Posicion = 0;
+            if (i > 0)
+            {
+                Posicion = Arreglo[i - 1];
+                Console.Write(Posicion + " ");
+                Inverso(i - 1, Arreglo);
+            }
+        }
+    }
+}
